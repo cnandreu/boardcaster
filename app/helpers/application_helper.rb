766f8f93e61fa0716af033bb55a1c35801527f4e
Carlos Andreu <carlosniko@gmail.com>
2011-10-03 20:54:37 -0400

    static pages work, layout with bootstrap css framework

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  
+  def check_active(page_name)
+   return "class='active'" if params[:action] == page_name
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Boardcaster::Application.routes.draw do
-  get "pages/home"
+  
+  root :to => "pages#home"
+  
+  match '/contact', :to => 'pages#contact'
+  match '/help',    :to => 'pages#help'
+  match '/about',   :to => 'pages#about'
 
-  get "pages/contact"
-
-  get "pages/help"
-
-  get "pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.

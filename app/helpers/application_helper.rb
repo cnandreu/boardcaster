10ebdde4c5fe646dddea4903cf6e1aec6dfd8224
Carlos Andreu <carlosniko@gmail.com>
2011-10-03 22:41:26 -0400

    fixed title to boardcaster

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,13 @@ module ApplicationHelper
    return "class='active'" if params[:action] == page_name
   end
   
+  def title
+    base_title = "Boardcaster"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+  
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,12 +1,3 @@
 module PagesHelper
-
-  def title
-    base_title = "Boardcasting"
-    if @title.nil?
-      base_title
-    else
-      "#{base_title} | #{@title}"
-    end
-  end
   
 end

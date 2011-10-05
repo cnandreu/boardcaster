1c79a7cfe4720ea56dd8429efc0c522757f5ac1f
Carlos Andreu <carlosniko@gmail.com>
2011-10-05 03:23:43 -0400

    red message for invalid username or pass for the login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
     if user
       redirect_back_or_to root_url, :success => "Logged in!"
     else
-      flash.now.alert = "Email or password was invalid"
+      flash.now[:error] = "Email or password was invalid"
       render :new
     end
   end

8c58438025bb460707667e595209dc3c4e106648
Carlos Andreu <carlosniko@gmail.com>
2011-10-07 22:21:26 -0400

    sexy jquery animation for success and notice messages

--- app/assets/javascripts/jchess-app.js
+++ app/assets/javascripts/jchess-app.js
@@ -1,5 +1,12 @@
 jQuery(document).ready(function() {
 
+  setTimeout($('.success, .notice').fadeOut(5000), 10000);
+
   var chess2 = jQuery('#board').chess({fen : "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2"});
+
+  
+
  
 })
+
+

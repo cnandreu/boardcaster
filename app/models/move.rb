20004ba4fa505359ba60cb84446159ef7fcb9b3e
Carlos Andreu <carlos.andreu@upr.edu>
2011-10-30 18:09:37 -0400

    moves validation model

--- app/models/move.rb
+++ app/models/move.rb
@@ -14,8 +14,24 @@ class Move < ActiveRecord::Base
   
   attr_accessible :move_data
   
-  belongs_to :player
   belongs_to :game
+
+  fen_regex = /[\w]+\/[\w]+\/[\w]+\/[\w]+\/[\w]+\/[\w]+\/[\w]+\/[\w]+\s.\s[\w]+\s[\w|-]+\s[\w]\s[\w]+/i
+
+  validates :move_data, :presence => true,
+                        :length => {:maximum => 255,
+                                    :minimum => 28 },
+                        :format => { :with => fen_regex }
+ 
+  validates :game_id,       :presence => true,
+                            :numericality => true
+
+  validate :existence
+    
+  def existence
+    errors.add(:game, "must exist") if Game.find_by_id(game_id).nil? 
+  end
+
 
 end
 

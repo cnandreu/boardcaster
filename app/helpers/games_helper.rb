ec22a04ca8102084c138ec9b6d000b45006862ab
xercesblue <dlcs.frank@gmail.com>
2011-11-09 14:43:41 -0400

    Added fen2alg helper.

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,28 @@
 module GamesHelper
+
+def fen2alg(fens)
+moves = []
+CHAR='_'
+fens.map! { |fen| fen[0,fen.index(/\s/)].split('/').each { |rank|
+rank.gsub!(/\d/) {|d| CHAR*Integer(d)}}}.each_cons(2) { |a, b| rank=0
+  a.zip(b).each do |ra, rb| 8.times {|file| ra_chr = ra[file]
+      rb_chr = rb[file]
+      act_rank = 8-rank
+      act_file = (file+65).chr
+      if ra_chr != rb_chr
+        if ra_chr == CHAR
+          if rb_chr.downcase == 'p'
+            moves.push("#{act_file}#{act_rank}")
+          else
+            moves.push("#{rb_chr}#{act_file}#{act_rank}")
+          end
+        end
+      end
+    }
+    rank+=1
+  end
+}
+moves
+end
+
 end

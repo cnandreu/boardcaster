44ea7b8f93330db041f0448dec49133f84687b71
Carlos Andreu <carlos.andreu@upr.edu>
2011-11-27 18:51:34 -0400

    commented jchess-app.js

--- app/assets/javascripts/jchess-app.js
+++ app/assets/javascripts/jchess-app.js
@@ -1,16 +1,29 @@
+/* 
+ * @author: Carlos Andreu
+ *
+ * This file includes the necessary code to work with live games. If the board
+ * has a "board-live" div class tag, it calls /moves via GET, it clears the
+ * old board and updates the current empty board with the new FEN. If it gets
+ * "***" that means the game is over and it sends a request to /mgame where the
+ * game will be marked live=false and the user will be redirected to the last
+ * game page and controls for 'Next Move", "Previous Move" and "Flip Board"
+ * will appear below the board. 
+ */
+
 jQuery(document).ready(function() {
+    
+    //Fade notification bars (ie. "Login Successful" green bars)
+    setTimeout($('.success, .notice').fadeOut(5000), 15000);
 
     if(document.getElementById("board-live")!=null){	
 	   setInterval(function() {	
 		
-		
-   
 		$.get("/moves", function(result){
 
             if(result == "***" && window.location.href.substr(-2) !== "?r"){
                 window.location="/mgame";
-                /*window.location = window.location.href + "?r"; */
-                
+                /* Old way of redirecting after the game was over:
+                 * window.location = window.location.href + "?r"; */        
             }
 
             $('#board-live').chess({fen : "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"}).clearBoard();
@@ -21,41 +34,28 @@ jQuery(document).ready(function() {
 
 		      $('#board-live').chess(myobj);
 		    });
-        
-		
-	   }, 1000);
+	   }, 1000); //Update interval in milliseconds, polls the DB for a new move.
 	   $.ajaxSetup({ cache: false });
     }
 
-  //setTimeout($('.success, .notice').fadeOut(5000), 15000);
+    //Code for displaying replays
+    var chess = jQuery('#board').chess({pgn : jQuery('#pgn-string').html()});
 
-/*
-  var chess2 = $('#board').chess({fen : "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2"});
-
-*/
-
-
-  var chess = jQuery('#board').chess({pgn : jQuery('#pgn-string').html()});
-
-  jQuery('#board-back').click(function() {
-    chess.transitionBackward();
-    jQuery("#board-annot").text( chess.annotation() );
-    return false;
-  });
+    jQuery('#board-back').click(function() { //previous move functionality
+        chess.transitionBackward();
+        jQuery("#board-annot").text( chess.annotation() );
+        return false;
+    });
   
-  jQuery('#board-next').click(function() {
-    chess.transitionForward();
-    jQuery("#board-annot").text( chess.annotation() );
-    return false;
-  });
+    jQuery('#board-next').click(function() { //next move functionality 
+        chess.transitionForward();
+        jQuery("#board-annot").text( chess.annotation() );
+        return false;
+    });
   
-  jQuery('#board-flip').click(function() {
-    chess.flipBoard();
-    return false;
-  });  
+    jQuery('#board-flip').click(function() { //flip board functionality
+        chess.flipBoard();
+        return false;
+    });  
 
-
-
-})
-
-
+}) //end document.ready
